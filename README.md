075f62cf184d55bb117823c8f5d41f8e8b336159
jiyinyiyong <jiyinyiyong@gmail.com>
2018-12-29 01:52:29 +0800

    initializing from workflow

---
+++ README.md
@@ -0,0 +1,17 @@
+
+Calcit Theme
+----
+
+> Color schemes library for calcit editor.
+
+### Usage
+
+TODO
+
+### Workflow
+
+Workflow https://github.com/mvc-works/calcit-workflow
+
+### License
+
+MIT
---
+++ src/app/config.cljs
@@ -0,0 +1,25 @@
+
+(ns app.config )
+
+(def cdn?
+  (cond
+    (exists? js/window) false
+    (exists? js/process) (= "true" js/process.env.cdn)
+    :else false))
+
+(def dev?
+  (let [debug? (do ^boolean js/goog.DEBUG)]
+    (cond
+      (exists? js/window) debug?
+      (exists? js/process) (not= "true" js/process.env.release)
+      :else true)))
+
+(def site
+  {:dev-ui "http://localhost:8100/main.css",
+   :release-ui "http://cdn.tiye.me/favored-fonts/main.css",
+   :cdn-url "http://cdn.tiye.me/calcit-theme/",
+   :cdn-folder "tiye.me:cdn/calcit-theme",
+   :title "Calcit Theme",
+   :icon "http://cdn.tiye.me/logo/cirru.png",
+   :storage-key "calcit-theme",
+   :upload-folder "tiye.me:repo/Cirru/calcit-theme/"})
